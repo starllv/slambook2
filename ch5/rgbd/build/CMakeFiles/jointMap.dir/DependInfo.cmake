34aba9cc660029c744109d547d47495a5a6c83c2
starllv <zhangqin37@aliyun.com>
2024-09-10 22:38:12 +0800

    初始化仓库

---
+++ ch5/rgbd/build/CMakeFiles/jointMap.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/embedfire/slambook2/ch5/rgbd/jointMap.cpp" "/home/embedfire/slambook2/ch5/rgbd/build/CMakeFiles/jointMap.dir/jointMap.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  "/usr/include/eigen3"
+  "/usr/local/include"
+  "/home/embedfire/Git_repo/slambook2/3rdparty/Sophus"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ch5/rgbd/build/CMakeFiles/jointMap.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jointMap.dir/jointMap.cpp.o"
+  "jointMap.pdb"
+  "jointMap"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jointMap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
